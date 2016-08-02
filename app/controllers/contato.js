259b8293d5cc8e54f35942fd76b8fe52cf50216d
Aristeu da Silva Filho <aristeusfilho@gmail.com>
2016-08-02 15:49:32 +0000

    Capitulo 5 Terminado

--- app/controllers/contato.js
+++ app/controllers/contato.js
@@ -1,33 +1,65 @@
+var ID_CONTATO_INC = 3;
+
 var contatos = [
-		{_id: 1, nome: 'Contato 1', email: 'cont1@empresa.com'},
-		{_id: 2, nome: 'Contato 2', email: 'cont2@empresa.com'},
-		{_id: 3, nome: 'Contato 3', email: 'cont3@empresa.com'},
-	];
+  {_id: 1, nome: 'Contato Exemplo 1', email: 'cont1@empresa.com.br'},
+  {_id: 2, nome: 'Contato Exemplo 2', email: 'cont2@empresa.com.br'},
+  {_id: 3, nome: 'Contato Exemplo 3', email: 'cont3@empresa.com.br'}
+]; 
 
-module.exports = function(){
-	var controller = {};
-	controller.listaContatos = function(req, res){
-		res.json(contatos);
-	};
+module.exports = function() {
+  
+  var controller = {};
+  
+  controller.listaContatos = function(req, res) {
+      res.json(contatos);
+  };
 
-	controller.obtemContato = function(req, res){
-		var idContato = req.params.id;
-		var contato = contatos.filter(function(contato){
-			return contato._id = idContato;
-		})[0];
+  controller.obtemContato = function(req, res) {
+    
+  	var idContato = req.params.id;
+  	var contato = contatos.filter(function(contato) {
+  		return contato._id == idContato;
+  	})[0];
+  	contato ? 
+  	res.json(contato):
+    res.status(404).send('Contato não encontrado');
+    
+  };
 
-		contato ?
-			res.json(contato):
-			res.status(404).send('Contato não encontrado');
-	};
+  controller.removeContato = function(req, res) {
+    var idContato = req.params.id;
+    contatos = contatos.filter(function(contato) {
+      return contato._id != idContato;
+    });
+    res.status(204).end();
+  };
 
-	controller.removeContato = function(req, res) {
-		var idContato = req.params.id;
-		contatos = contatos.filter(function(contato) {
-			return contato._id != idContato;
-		});
-		res.status(204).end();
-	};
+  controller.salvaContato = function(req, res) {
 
-	return controller;
-}
+    var contato = req.body;
+    contato = contato._id ?
+      atualiza(contato) :
+      adiciona(contato);
+    res.json(contato);
+  };
+
+  function adiciona(contatoNovo) {
+
+    contatoNovo._id = ++ID_CONTATO_INC;;
+    contatos.push(contatoNovo);
+    return contatoNovo;
+  }
+
+  function atualiza(contatoAlterar) {
+
+    contatos = contatos.map(function(contato) {
+      if(contato._id == contatoAlterar._id) {
+        contato = contatoAlterar;
+      }
+      return contato;
+    });
+    return contatoAlterar;
+  }
+
+  return controller;
+};
--- app/routes/contato.js
+++ app/routes/contato.js
@@ -1,9 +1,12 @@
-module.exports = function(app){
-	var controller = app.controllers.contato;
-	app.route('/contatos')
-		.get(controller.listaContatos);
+module.exports = function (app) {
 	
-	app.route('/contatos/:id')
-		.get(controller.obtemContato)
-		.delete(controller.removeContato);
+  var controller = app.controllers.contato;
+
+  app.route('/contatos')
+  	.get(controller.listaContatos)
+  	.post(controller.salvaContato)
+
+  app.route('/contatos/:id')
+	.get(controller.obtemContato)
+	.delete(controller.removeContato);
 };
